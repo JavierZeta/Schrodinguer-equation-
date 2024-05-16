04f356ab27ac8679aa0b62caec5ec1301a305db2
victoor7 <124279363+victoor7@users.noreply.github.com>
2024-05-17 00:02:33 +0200

    Delete Matlab directory

--- Matlab/potencial.m
+++
@@ -1,8 +0,0 @@
-function [Vpot] = potpozofinito(x)
-
-    if abs(x) > 20
-        Vpot = -50;
-    else 
-        Vpot = 0;
-    end
-end
--- Matlab/potpozofinito.m
+++
@@ -1,8 +0,0 @@
-function [Vpot] = potpozofinito(x)
-
-    if abs(x) >5
-        Vpot = 10;
-    else 
-        Vpot = 0;
-    end
-end
