9979a097617c6bb5a048c95a23ec60aa39e9cd3e
Nacho-88 <124189251+Nacho-88@users.noreply.github.com>
2024-05-16 22:58:07 +0200

    Add files via upload

--- THE_CARPET/calculo_error_finito.m
+++ THE_CARPET/calculo_error_finito.m
@@ -1,24 +1,24 @@
-function [error_f] = calculo_error_finito(v,vecnorm,numerovec,energias,a,b) 
+function [error_f] = calculo_error_finito(v,vecnorm,numerovec,energias,a,b,N) 
 
 error_f = zeros(numerovec, 1); %Este vector sera el que devolvamos
 
-a=funcion_a_trozos(v,numerovec,energias,a,b);
+e=funcion_a_trozos(v,numerovec,energias,a,b);
 
 figure
 
 for i=1 : numerovec
 
-    plot(v,a(:,i)+i)
+    plot(v,e(:,i)-i)
 
     hold on
 
     legendInfo{i} = sprintf('Funcion de onda %d', i);
   
-    b=a(:,i); %La quiero como vector
+    g=e(:,i); %La quiero como vector
 
     c=vecnorm(:,i); %Esto es un vector de n-2,1
 
-    error=abs(abs(b)-abs(c)); %Calculamos el error punto por punto
+    error=abs(abs(g)-abs(c)); %Calculamos el error punto por punto
 
     d=max(error(:,1));
 
@@ -28,7 +28,17 @@ for i=1 : numerovec
 
 end
 
+[x,pozo]=dibujo_pozo_infinito(a,b,N);
+
+plot(x,pozo,"LineWidth",1,"DisplayName","Pozo finito","Color","k")
+
+xlim([-8,8])
+
+ylim([-5.5,1])
+
 legend(legendInfo);
-title("Funciones de onda analíticas")
+title("Funciones de onda analíticas en el pozo de potencial finito")
+xlabel("x")
+ylabel("\psi_n(x)")
 
 end
