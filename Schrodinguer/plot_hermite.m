e76ab19d1dfaee31d44117c05c59daa043678833
victoor7 <124279363+victoor7@users.noreply.github.com>
2024-05-17 00:02:44 +0200

    Delete Schrodinguer directory

--- Schrodinguer/plot_hermite.m
+++
@@ -1,9 +0,0 @@
-function plot_hermite 
-syms x
-fplot(hermiteH(0:4,x))
-axis([-2 2 -30 30])
-grid on
-ylabel('H_n(x)')
-legend('H_0(x)', 'H_1(x)', 'H_2(x)', 'H_3(x)', 'H_4(x)', 'Location', 'Best')
-title('Polinomios de Hermite')
-end
