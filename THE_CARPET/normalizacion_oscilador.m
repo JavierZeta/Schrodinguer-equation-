63e5f8ceb9faf8c0449c17814eb07b1d021500fa
victoor7 <124279363+victoor7@users.noreply.github.com>
2024-05-19 02:49:05 +0200

    Delete THE_CARPET directory

--- THE_CARPET/normalizacion_oscilador.m
+++
@@ -1,7 +0,0 @@
-function[vecnorm] = normalizacion_oscilador(vec,area,numerovec,tramos)
-vecnorm=zeros(tramos,tramos);
-for j=1:numerovec
-for i = 1:tramos
-vecnorm(i,j)=vec(i,j)/area(j);
-end
-end
--- THE_CARPET/regla_trapecizoidal.m
+++
@@ -1,7 +0,0 @@
-function area = regla_trapecizoidal(vec,h,numerovec)
-area=zeros(numerovec,1);
-for i= 1:numerovec
-columna=vec(:,i).^2;
-area(i)=(h/2)*(vec(1,i)+vec(end,i)+2*sum(columna(2:end-1)));
-area(i) = sqrt(area(i));
-end
